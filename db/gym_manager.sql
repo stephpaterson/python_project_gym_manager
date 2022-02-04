870a82b4556e7d1538a51f0c337c28a3cf302fc0
Steph Paterson <steph.c.paterson@gmail.com>
2022-02-04 13:54:37 +0000

    basic file structure and tables created

---
+++ db/gym_manager.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS members
+DROP TABLE IF EXISTS gym_classes
+
+CREATE TABLE members (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255)
+);
+
+CREATE TABLE gym_classes (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    location VARCHAR(255),
+    instructor VARCHAR(255),
+    date VARCHAR(255),
+    time VARCHAR(255)
+);
