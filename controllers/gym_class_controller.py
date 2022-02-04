a878a39c9172e0809091ec390438d4213f039771
Steph Paterson <steph.c.paterson@gmail.com>
2022-02-04 16:29:49 +0000

    basic form that adds a new class complete

--- controllers/gym_class_controller.py
+++ controllers/gym_class_controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.gym_class import GymClass
 import repositories.gym_class_repository as gym_class_repo
@@ -9,3 +9,18 @@ gym_classes_blueprint = Blueprint("gym_classes", __name__)
 def gym_classes():
     gym_classes = gym_class_repo.select_all()
     return render_template("gym_class/index.html", gym_classes=gym_classes)
+
+@gym_classes_blueprint.route("/gym_classes/new", methods = ['GET'])
+def gym_class_new():
+    return render_template("gym_class/new.html")
+
+@gym_classes_blueprint.route("/gym_classes", methods=['POST'])
+def create_gym_class():
+    name = request.form['name']
+    instructor = request.form['instructor']
+    location = request.form['location']
+    date = request.form['date']
+    time = request.form['time']
+    gym_class = GymClass(name, instructor, location, date, time)
+    gym_class_repo.save(gym_class)
+    return redirect('/gym_classes')
