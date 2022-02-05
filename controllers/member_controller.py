f75d89b0bf70e4d8ca8d083b69ce2dc4a3c03b15
Steph Paterson <steph.c.paterson@gmail.com>
2022-02-05 13:45:46 +0000

    can successfully add new member

--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -5,7 +5,21 @@ from models.member import Member
 
 members_blueprint = Blueprint("members", __name__)
 
-@members_blueprint.route('/members')
+@members_blueprint.route('/members', methods=['GET'])
 def show():
     members = member_repo.select_all()
     return render_template('/member/index.html', members=members)
+
+@members_blueprint.route('/members/new', methods=['GET'])
+def new_member():
+    return render_template('/member/new.html')
+
+@members_blueprint.route('/members', methods=['POST'])
+def create_member():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    phone_number = request.form['phone_number']
+    email = request.form['email']
+    member = Member(first_name, last_name, phone_number, email)
+    member_repo.save(member)
+    return redirect('/members')
